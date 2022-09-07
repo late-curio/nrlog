5f88539b2da8ac9556ea5b65344d2fc4ee55ea3c
twcrone <twcrone@gmail.com>
2022-09-07 07:04:37 -0400

    Add transaction count

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ crystal build nrlog.cr --release
 echo "Creating a ~/bin directory if it does not already exist"
 mkdir ~/bin
 echo "Copying to ~/bin"
+rm ~/bin/nrl
 cp nrlog ~/bin/nrl
 echo "Make sure to add your ~/bin directory to your PATH"
 echo "Once that is done, you should be able to access the NR Log Parser via 'nrl'"
--- nrlog.cr
+++ nrlog.cr
@@ -4,6 +4,8 @@ if ARGV.size() > 0
     file = ARGV[0]
 end
 weaved = Set(String).new()
+transaction_count = 0
+transaction_activity = Array(String).new()
 File.each_line(file) do |line|
     if line.includes?("weaved")
         i = line.index(".instrumentation.")
@@ -14,7 +16,11 @@ File.each_line(file) do |line|
             end  
         end
     end
+    if line.includes?("TransactionActivity@") && line.includes?("starting")
+        transaction_count += 1
+    end
 end
+puts "#{transaction_count} transactions started"
 weaved.to_a().sort().each do |pkg|
     puts pkg
 end

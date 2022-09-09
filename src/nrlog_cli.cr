c003c4e45e9a9766361074a301bde602a0eed585
twcrone <twcrone@gmail.com>
2022-09-08 21:51:04 -0400

    Add some more CLI prettiness

--- src/nrlog_cli.cr
+++ src/nrlog_cli.cr
@@ -6,17 +6,34 @@ if ARGV.size() > 0
 end
 log = Nrlog::AgentLog.new(file)
 log.load()
-session = log.first
-puts "#{session.transaction_count} transactions started"
-puts ""
-puts "Instrumentation"
-puts "---------------"
-session.weaved.to_a().sort().each do |pkg|
-  puts pkg
-end
-puts ""
-puts "Extensions (not loaded initially)"
-puts "---------------------------------"
-session.extensions.to_a().sort().each do |pkg|
-  puts pkg
+
+puts "#{log.sessions.size} sessions started"
+
+count = 0
+
+log.sessions.each do |session|
+  count += 1
+  puts ""
+  puts "[Session #{count}]"
+  puts "#{session.transaction_count} transactions started"
+  instrumentation = session.weaved
+  if instrumentation.size > 0
+    puts "Instrumentation"
+    puts "---------------"
+    session.weaved.to_a().sort().each do |pkg|
+      puts pkg
+    end
+  else
+    puts "*** No Instrumentation Modules Loaded ***"
+  end
+  
+  extensions = session.extensions
+  if extensions.size > 0
+    puts ""
+    puts "Extensions (not loaded initially)"
+    puts "---------------------------------"
+    session.extensions.to_a().sort().each do |pkg|
+      puts pkg
+    end
+  end
 end

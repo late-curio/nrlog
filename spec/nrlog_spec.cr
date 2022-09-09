44fe3d1646dc2e4e7aadc868361a543d49cac95b
twcrone <twcrone@gmail.com>
2022-09-08 21:06:18 -0400

    Add a proper spec to read the sample log

--- spec/nrlog_spec.cr
+++ spec/nrlog_spec.cr
@@ -2,12 +2,13 @@ require "./spec_helper"
 
 describe Nrlog do
 
-  it "works" do
+  it "should read a sample log and get correct statistics" do
     log = Nrlog::AgentLog.new("./spec/logs/nr.log")
     log.load()
     session = log.first
 
     session.transaction_count.should eq 200
-
+    session.weaved.size.should eq 7
+    session.extensions.size.should eq 1
   end
 end
